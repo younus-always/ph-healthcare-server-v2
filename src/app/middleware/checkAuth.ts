64825b9b7e13165796f8da824736db8ac084ff05
younus-always <younusofficial443@gmail.com>
2026-08-27 22:33:46 +0600

    feat: create authorization middleware and protecting specialty routes

---
+++ src/app/middleware/checkAuth.ts
@@ -0,0 +1,86 @@
+import { NextFunction, Request, Response } from "express";
+import { Role, UserStatus } from "../../generated/prisma/enums";
+import { cookieUtils } from "../utils/cookie";
+import AppError from "../errorHelpers/AppError";
+import status from "http-status";
+import { prisma } from "../lib/prisma";
+import { jwtUtils } from "../utils/jwt";
+import { envVars } from "../config/env";
+
+export const checkAuth = (...authRoles: Role[]) => async (req: Request, res: Response, next: NextFunction) => {
+      try {
+            // Session Token Verification
+            const sessionToken = cookieUtils.getCookie(req, "better-auth.session_token");
+
+            if (!sessionToken) {
+                  throw new AppError(status.UNAUTHORIZED, "Unauthorized access! No session token provided.")
+            };
+
+            if (sessionToken) {
+                  const sessionExists = await prisma.session.findFirst({
+                        where: {
+                              token: sessionToken,
+                              expiresAt: {
+                                    gt: new Date()
+                              }
+                        },
+                        include: {
+                              user: true
+                        }
+                  });
+
+                  if (sessionExists && sessionExists.user) {
+                        const user = sessionExists.user;
+
+                        const now = new Date();
+                        const createdAt = new Date(sessionExists.createdAt);
+                        const expiresAt = new Date(sessionExists.expiresAt);
+
+                        const sessionLifeTime = expiresAt.getTime() - createdAt.getTime();
+                        const timeRemaining = expiresAt.getTime() - now.getTime();
+                        const percentRemaining = (timeRemaining / sessionLifeTime) * 100;
+
+                        if (percentRemaining < 20) {
+                              res.setHeader("X-Session-Refresh", "true");
+                              res.setHeader("X-Session-Expires-At", expiresAt.toISOString());
+                              res.setHeader("X-Time-Remaining", timeRemaining.toString());
+
+                              console.log("Session Expiring Soon!");
+                        };
+
+                        if (user.status === UserStatus.BLOCKED || user.status === UserStatus.DELETED) {
+                              throw new AppError(status.UNAUTHORIZED, "Unauthorized access! User is not active.")
+                        };
+
+                        if (user.isDeleted) {
+                              throw new AppError(status.NOT_FOUND, "User is deleted.");
+                        };
+
+                        if (authRoles.length > 0 && !authRoles.includes(user.role)) {
+                              throw new AppError(status.FORBIDDEN, "Forbidden access! You don't have permission to access this resource.")
+                        };
+                  };
+            };
+
+            // Access Token Verification
+            const accessToken = cookieUtils.getCookie(req, "accessToken");
+
+            if (!accessToken) {
+                  throw new AppError(status.UNAUTHORIZED, "Unauthorized access! No access token provided.");
+            };
+
+            const verifiedToken = jwtUtils.verifyToken(accessToken, envVars.ACCESS_TOKEN_SECRET);
+
+            if (!verifiedToken.success) {
+                  throw new AppError(status.UNAUTHORIZED, "Unauthorized access! Invalid access token.");
+            };
+
+            if (authRoles.length > 0 && !authRoles.includes(verifiedToken.data!.role as Role)) {
+                  throw new AppError(status.FORBIDDEN, "Forbidden access! You don't have permission to access this resource.")
+            };
+
+            next();
+      } catch (err: any) {
+            next(err);
+      }
+};
