648b95dddd09c912283215772aaa3800a75d1f21
younus-always <younusofficial443@gmail.com>
2026-08-29 21:24:59 +0600

    feat: creating function to upload and delete file from cloudinary

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -5,11 +5,21 @@ import { ZodError } from "zod";
 import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
 import { handleZodError } from "../errorHelpers/handleZodError";
 import AppError from "../errorHelpers/AppError";
+import { deleteFileFromCloudinary } from "../config/cloudinary.config";
 
 
-export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+export const globalErrorHandler = async (err: any, req: Request, res: Response, next: NextFunction) => {
       if (envVars.NODE_ENV === "development") {
             console.log("Error from Global Error Handler:", err);
+      };
+
+      if (req.file) {
+            await deleteFileFromCloudinary(req.file.path)
+      };
+
+      if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+            const imageUrls = req.files.map((file) => file.path);
+            await Promise.all(imageUrls.map(url => deleteFileFromCloudinary(url)));
       };
 
       let statusCode: number = status.INTERNAL_SERVER_ERROR;
